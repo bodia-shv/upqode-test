830a00e452cfddcc7937e85df1d639e6d44bf3a4
Bodia Shvaguliak <bodia1994shv@gmail.com>
2017-09-23 12:49:16 +0300

    Update 2017-09-23T09:48:54.430Z

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+(function(window, document, undefined) {
+
+// Google Map API
+
+    ( function initMap() {
+        // Create a map object and specify the DOM element for display.
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      })
+
+
+})(window, document);
